9ecb112aef910a81ad71ebfc32da779e5ab32580
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-02-10 20:46:38 -0500

    added on key funtion in js code

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+// Array listing out rock, paper, scissors options
+var computerChoices = ["r", "p", "s"];
 
+// Whenever a key is pressed this function will run
+document.onkeyup = function(event) {
+
+    // Determines which key was pressed
+    var userGuess = event.key;
+
+    // Randomly chooses a choice from array aka computer's guess. 
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
 
 });
