452af622c8581bde8b803ee89dc78c88e472bce7
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-07 23:20:19 -0500

    changes to the start game function and ref

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
     apiKey: "AIzaSyCPby64S1gkCpFZeYkafKfaqDNN61AB4f4",
-  authDomain: "rps-multiplayer-game-c8781.firebaseapp.com",
-  databaseURL: "https://rps-multiplayer-game-c8781.firebaseio.com",
-  projectId: "rps-multiplayer-game-c8781",
-  storageBucket: "rps-multiplayer-game-c8781.appspot.com",
-  messagingSenderId: "675424472021",
-  appId: "1:675424472021:web:c7435b115b86f4e02b0b5c"
+    authDomain: "rps-multiplayer-game-c8781.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-game-c8781.firebaseio.com",
+    projectId: "rps-multiplayer-game-c8781",
+    storageBucket: "rps-multiplayer-game-c8781.appspot.com",
+    messagingSenderId: "675424472021",
+    appId: "1:675424472021:web:c7435b115b86f4e02b0b5c"
   };
 
   // Initialize Firebase
@@ -19,101 +19,127 @@ var firebaseConfig = {
 
   var connectionsRef = database.ref("/connections");
 
-// '.info/connected' is a special location provided by Firebase that is updated every time
-// the client's connection state changes.
-// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-var connectedRef = database.ref(".info/connected");
+  // '.info/connected' is a special location provided by Firebase that is updated every time
+  // the client's connection state changes.
+  // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+  var connectedRef = database.ref(".info/connected");
 
-// When the client's connection state changes...
-connectedRef.on("value", function(snap) {
+  // When the client's connection state changes...
+  connectedRef.on("value", function (snap) {
 
-  // If they are connected..
-  if (snap.val()) {
+    // If they are connected..
+    if (snap.val()) {
 
- 
-    // // Add user to the connections list.
-    var con = connectionsRef.push(true) 
-    // console.log(count);
-    // count++
-    // Remove user from the connection list when they disconnect.
-    con.onDisconnect().remove();
-  }
-});
+
+      // // Add user to the connections list.
+      var con = connectionsRef.push(true)
+      // console.log(count);
+      // count++
+      // Remove user from the connection list when they disconnect.
+      con.onDisconnect().remove();
+    }
+  });
+
+  // When first loaded or when the connections list changes...
+  connectionsRef.on("child_added", function (snapshot) {
+
+    // Display the viewer count in the html.
+    // The number of online users is the number of children in the connections list.
+
+    if (snapshot.val().name != undefined)
+      $("#players").append(snapshot.val().name + "<br>")
+  });
+
+
+  // Array listing out rock, paper, scissors options
+  var computerChoices = ["r", "p", "s"];
+
+  // Variables to hold number of wins, losses and ties
+  var wins = 0;
+  var losses = 0;
+  var ties = 0;
+
+  // Create variables in which you'd like to display the user choices & computers choices in the html document
+  var gameDirections = document.getElementById("game-directions");
+  var userChoiceAnswer1 = document.getElementById("userchoice1-answer");
+  var userChoiceAnswer2 = document.getElementById("userchoice2-answer");
+  var computerChoiceAnswer = document.getElementById("computerchoice-answer");
+  var winsText = document.getElementById("wins-text");
+  var lossesText = document.getElementById("losses-text");
+  var tiesText = document.getElementById("ties-text");
+
+  var addUser = document.getElementById("adduser");
+
+  var uniqueID = ""
+
+  addUser.addEventListener("click", function () {
+    var player = document.getElementById("text").value
+
+
+    if (player.length > 0) {
+      // Add user to the connections list.
+      uniqueID = connectionsRef.push({
+        name: player
+      })
+    }
+  })
 
 // When first loaded or when the connections list changes...
-connectionsRef.on("child_added", function(snapshot) {
+connectionsRef.on("value", function(snapshot) {
 
   // Display the viewer count in the html.
   // The number of online users is the number of children in the connections list.
-   
-if(snapshot.val().name!=undefined)
-  $("#players").append(snapshot.val().name +"<br>")
-});
-
-
-// Array listing out rock, paper, scissors options
-var computerChoices = ["r", "p", "s"];
-
-// Variables to hold number of wins, losses and ties
-var wins = 0;
-var losses = 0;
-var ties = 0;
-
-// Create variables in which you'd like to display the user choices & computers choices in the html document
-var gameDirections = document.getElementById("game-directions");
-var userChoiceAnswer1 = document.getElementById("userchoice1-answer");
-var userChoiceAnswer2 = document.getElementById("userchoice2-answer");
-var computerChoiceAnswer = document.getElementById("computerchoice-answer");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var tiesText = document.getElementById("ties-text");
-
-var addUser = document.getElementById("adduser");
-
-document.addEventListener("click", function() {
-    var player = document.getElementById("text").value
+  
+  var numUser = snapshot.numChildren()
+  console.log(numUser);
+    if (numUser >= 2 ){
+      startGame()
+    }
  
-    
-      if(player.length>0){
-  // Add user to the connections list.
-  var con = connectionsRef.push({ 
-    name: player
-  }) 
+});
   
-  
-      }
-      
-      
-})
 
-// Whenever a key is pressed this function will run
-document.onkeyup = function(event) {
 
-    // Determines which key was pressed
-    var userGuess = event.key;
+  function startGame() {
 
-    // Randomly chooses a choice from array aka computer's guess. 
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    // Whenever a key is pressed this function will run
+    document.onkeyup = function (event) {
 
-    // Runs code if the user presses "r" or "p" or "s".
-    if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+      // Determines which key was pressed
+      var userGuess = event.key;
+
+      // Randomly chooses a choice from array aka computer's guess. 
+      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+      // Runs code if the user presses "r" or "p" or "s".
+      if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+        console.log(uniqueID)
+        if (uniqueID.length != 0 ) {
+          console.log(uniqueID.getKey())
+          database.ref("/connections/" + uniqueID.getKey()).push({
+            userGuess: userGuess
+          })
+
+        }
+     
+        $("#players").html(userGuess)
 
         // If the user chooses r, p or s and the computer guess another variable, increases or decreases wins/losses or ties.
         if ((userGuess === "r") && (computerGuess === "s")) {
-            wins++;
+          wins++;
         }
 
         if ((userGuess === "s") && (computerGuess === "p")) {
-            wins++;
+          wins++;
         }
 
         if ((userGuess === "p") && (computerGuess === "r")) {
-            wins++;
+          wins++;
 
         } else if (userGuess === computerGuess) {
-            ties++;
+          ties++;
         } else {
-            losses++;
+          losses++;
         }
 
         // Hide directions after game starts (user + computer guesses)
@@ -126,7 +152,12 @@ document.onkeyup = function(event) {
         winsText.textContent = "wins: " + wins;
         lossesText.textContent = "losses: " + losses;
         tiesText.textContent = "ties: " + ties;
+      }
     }
-}
+  }
+
+
+
+
 
 });
